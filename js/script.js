c3e33a0fdc18510a521228b9447baf5782e14f18
Milinda Kaushika <milindakaushika@outlook.com>
2023-02-04 07:43:59 -0800

    added js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Mobile responsive naverbar sticky
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 100);
+});
